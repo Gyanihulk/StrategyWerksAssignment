7e09dfc680ff0d61bb83d4e44f412ea037fd8dc9
GyaniHulk <adamya@appfoster.com>
2024-09-19 23:42:39 +0530

    feat/github pages deploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import pokemonReducer from './pokemonSlice';
+import pokemonReducer from './pokemonslice';
 
 export const store = configureStore({
   reducer: {
